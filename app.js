6860ba5420140eacb54fcf4bbb7ea2f4fa5a7675
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-06-19 13:30:43 +0700

    create new endpoint to add new wallet

--- app.js
+++ app.js
@@ -15,16 +15,37 @@ app.get("/:file", function (req, res) {
 });
 
 // update file in database
-app.post("/:file", function (req, res) {
-  var file = req.params.file;
-  var data = req.body;
-  res.send("Updated " + file + " with " + data);
-  //   write file to database
-  fs.writeFile(file, JSON.stringify(data), function (err) {
+app.post("/addConnectedWallet/:username", function (req, res) {
+  var username = req.params.username;
+  var newwallet = req.body;
+  res.send("Updated " + username + " with " + JSON.stringify(newwallet));
+
+  // read from database
+  fs.readFile("database.json", function (err, data) {
     if (err) {
-      return console.log(err);
+      console.log(err);
+    } else {
+      var json = JSON.parse(data);
+      // iterate through json, find where username is equal to the username in the url
+      for (var i = 0; i < json.length; i++) {
+        if (json[i].username == username) {
+          // update the json
+          // append data to the json
+          console.log("json");
+          console.log(json);
+          json[i].connectedWallet.push(newwallet);
+          //   write file to database
+          console.log("json");
+          console.log(json);
+          fs.writeFile("database.json", JSON.stringify(json), function (err) {
+            if (err) {
+              return console.log(err);
+            }
+            console.log("The file was saved!");
+          });
+        }
+      }
     }
-    console.log("The file was saved!");
   });
 });
 
